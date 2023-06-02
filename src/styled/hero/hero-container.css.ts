b6b5b8e202069b5236c1dc778c72da1306f40f9a
Quentin Delettre <quentin.delettre@pm.me>
2023-06-03 00:08:12 +0200

    feat: last year sum (#6)

--- src/styled/hero/hero-container.css.ts
+++ src/styled/hero/hero-container.css.ts
@@ -2,6 +2,7 @@ import { styled } from "styled-vanilla-extract/qwik";
 
 export const HeroSpanContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
---
+++ src/utils/get-stats.ts
@@ -0,0 +1,26 @@
+interface Stats {
+  avg: number;
+  min: number;
+  max: number;
+}
+export const getStats = (data: number[]): Stats | never => {
+  if (data.length === 0) {
+    throw new Error();
+  }
+
+  const stats = data.reduce(
+    (acc, num) => {
+      acc.sum += num;
+      acc.min = Math.min(acc.min, num);
+      acc.max = Math.max(acc.max, num);
+      return acc;
+    },
+    { sum: 0, min: 0, max: 0 }
+  );
+
+  return {
+    avg: stats.sum / data.length,
+    min: stats.min,
+    max: stats.max,
+  };
+};
